319cc8755d752f70c0960c1ad872411dbf446ca5
Fenobi <13667739122@qq.com>
2022-09-16 22:04:02 +0800

    添加项目文件。

---
+++ fu.cpp
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include <iostream>
+
+using namespace std;
+
+//int& count()
+//{
+//	int n = 0;
+//	cout << &n << endl;
+//
+//	n++;
+//	return n;
+//}
+//
+//void fan()
+//{
+//	int x = 0;
+//	int y = 2;
+//	cout << &x << endl;
+//	cout << &y << endl;
+//
+//}
+//
+//int main()
+//{
+//	int &a = count();
+//	//int a = 10;
+////	int& ra = a;
+//
+//	cout << a << endl;
+//	cout << a << endl;
+//	fan();
+//	cout << a << endl;
+//
+//
+//	return 0;
+//
+//}
+
+//int main()
+//{
+//	double d1 = 1.1,d2=2.2;
+//	const int& a1 = d1;
+//	int& a2 = d2;//±¨´í
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a = 10;
+//	int* pa = &a;
+//	*pa = 20;
+//	
+//	int& ra = a;
+//	ra = 30;
+//	return 0;
+//}
